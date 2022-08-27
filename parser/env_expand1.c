488f8defdc3f80e91ab151e543f9375808871562
AfafTarchoun <74008660+AfafTarchoun@users.noreply.github.com>
2022-08-27 18:51:31 +0200

    Update env_expand1.c

--- parser/env_expand1.c
+++ parser/env_expand1.c
@@ -55,38 +55,89 @@ void	handle_env_replace(char **str, char *env, int flag, t_exec *exec)
 	free(placeholder);
 }
 
-char	*expand_env(char *str, char quote, t_exec *exec)
-{
-	char	*env;
 
-	if (quote == '\"')
-	{
-		while (find_char_index(str, '$') != -2)
-		{
-			env = find_str_env(str);
-			if (env[1] != '\0')
-				handle_env_replace(&str, env, 1, exec);
-			else
-				handle_env_replace(&str, env, 0, exec);
-		}
-		str = replace_char(str, '\200', '$');
-	}
-	return (str);
+char *handle_num_env(char *str)
+{
+  char *result;
+  int len;
+  int i;
+  int j;
+  
+  len = strlen(str);
+  result = (char *)malloc(sizeof(char) * (len - 2) + 1);
+  i = 2;
+  j = 0;
+  while(str[i])
+  {
+    result[j] = str[i];
+    i++;
+    j++;
+  }
+  result[j] = '\0';
+  return (result);
 }
 
-t_token	*lx_collect_env(t_lexer *lexer, t_exec *exec)
+void handle_num_replace(char **str, char *tmp, char *env)
 {
-	char	*value;
-	char	*str;
+  char *placeholder;
 
-	value = (char *)malloc(sizeof(char) + 1);
-	value[0] = '\0';
-	collect_process(&str, &value, lexer);
-	if (lexer->c == '_')
-		collect_process(&str, &value, lexer);
-	while (isalnum(lexer->c))
-		collect_process(&str, &value, lexer);
-	if (find_char_index(value, '$') != -2)
-		value = expand_env_word(value, 0, exec);
-	return (init_token(WORD, value, 0));
+  placeholder = *str;
+  *str = replace_env(*str, tmp, env);
+  free(env);
+  free(placeholder);
+}
+
+char *expand_env(char *str, char quote, t_exec *exec)
+{
+  char *tmp;
+  char *env;
+  char *placeholder;
+  
+  if (quote == '\"')
+  {
+    while (find_char_index(str, '$') != -2)
+    {
+      env = find_str_env(str);
+      if (ft_isdigit(env[1]))
+      {
+        tmp = env; 
+        env = handle_num_env(env);
+        handle_num_replace(&str, tmp, env);
+        free(tmp);
+      }
+      else if (env[1] != '\0')
+        handle_env_replace(&str, env, 1, exec);
+      else
+        handle_env_replace(&str, env, 0, exec);
+    }
+    str = replace_char(str, '\200', '$');
+  }
+  return (str);
+}
+
+t_token *lx_collect_env(t_lexer *lexer, t_exec *exec)
+{
+  char *value;
+  char *str;
+  char *tmp;
+
+  value = (char *)malloc(sizeof(char) + 1);
+  value[0] = '\0';
+  collect_process(&str, &value, lexer);
+  while (lexer->c == '_')
+    collect_process(&str, &value, lexer);
+  while (ft_isalnum(lexer->c))
+    collect_process(&str, &value, lexer);
+  if (find_char_index(value, '$') != -2)
+  {
+    if (ft_isdigit(value[1]))
+    {
+      tmp = value; 
+      value = handle_num_env(value);
+      free(tmp);
+    }
+    else
+      value = expand_env_word(value, 0, exec);
+  }
+  return (init_token(WORD, value, 0));
 }
